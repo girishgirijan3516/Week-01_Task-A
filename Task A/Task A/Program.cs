e80b9331455eeb1391f19b3f1dcc3f625ec89dea
Girish Vazheliparambil Girijan <738048@student.tor.loc>
2019-05-08 11:35:58 -0400

    Second commit

---
+++ Task A/Task A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, total = 0; // Declare and initialize variables
+            for (i = 314; i >= 102; i -= 6)
+            {      // Begining of loop  
+                   //Console.WriteLine (i);
+                total += i; // counting total
+            }
+            Console.WriteLine("Total = " + total); // Display total  
+        }
+    }
+}
